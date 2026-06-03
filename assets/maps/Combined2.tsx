f06efd5facbbf781e2616398e4f39e79ac44bf00
Duong Phu Trong <duongphutrong2007@gmail.com>
2026-06-03 09:19:10 +0700

    Add new maps

---
+++ assets/maps/Combined2.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Combined2" tilewidth="16" tileheight="16" tilecount="24" columns="12">
+ <image source="../../Ninja Adventure - Asset Pack/Backgrounds/Combined2.png" width="204" height="36"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+</tileset>

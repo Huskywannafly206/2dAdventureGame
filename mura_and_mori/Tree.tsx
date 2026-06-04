18fe3b72599ecd37930b13dd885dcbbbd406057d
Az <anhvu.az.p@gmail.com>
2026-06-04 15:39:26 +0700

    Map lang va map rung

---
+++ mura_and_mori/Tree.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Tree" tilewidth="64" tileheight="48" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="Object/3Tree.png" width="64" height="48"/>
+ </tile>
+ <tile id="1">
+  <image source="../2dAdventureGame-main/Ninja Adventure - Asset Pack/Items/Treasure/SilverKey.png" width="12" height="8"/>
+ </tile>
+ <tile id="3">
+  <image source="Object/training_dummy/damaged_down_00.png" width="32" height="32"/>
+ </tile>
+</tileset>

fa0a71fb01106a34752c731c4392a86aeaa58402
phongntn <phongntn1@gmail.com>
2026-06-07 16:54:32 +0700

    Fix item respawn bug

--- core/src/main/java/io/github/com/group31/system/ItemSystem.java
+++ core/src/main/java/io/github/com/group31/system/ItemSystem.java
@@ -114,6 +114,11 @@ public class ItemSystem extends IteratingSystem {
             );
         }
 
+        io.github.com.group31.component.Respawnable respawnable = entity.getComponent(io.github.com.group31.component.Respawnable.class);
+        if (respawnable != null) {
+            io.github.com.group31.save.RespawnState.getInstance().registerDeath(respawnable.getEntityId(), respawnable.getRespawnTimeSec());
+        }
+
         toRemove.add(entity);
     }
 
